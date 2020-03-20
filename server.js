0e2ceb66208089aabde36ad73b18027b8fcc2f69
Pakawan Bugger <62284566+smileu2za001@users.noreply.github.com>
2020-03-20 15:21:48 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const Comment = require('./models/comment');
 const seedDB = require('./seeds')
 
 const app = express();
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
